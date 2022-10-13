77284dedde3e6f9e64491d86b531204c48cd2c7e
Asus <Asus@10.104.1.213>
2022-10-13 23:33:36 +0800

    second commit

---
+++ secondqs/src/secondqs/Student.java
@@ -0,0 +1,12 @@
+package secondqs;
+
+public class Student 
+{
+	public static void main (String[] args)
+	{
+		System.out.println("Nur Hanie Elvira binti Hamran");
+		System.out.println("212681");
+		System.out.println("Kolej Canselor");
+	}
+
+}
