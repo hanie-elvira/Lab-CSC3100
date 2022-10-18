c13f54d3e95310e13368a873b07db210d0ca607b
Asus <Asus@10.104.1.213>
2022-10-18 21:32:16 +0800

    third commit

---
+++ questhree/src/questhree/Multiple.java
@@ -0,0 +1,11 @@
+package questhree;
+
+public class Multiple {
+	public static void main (String[] args)
+	{
+		System.out.println("Welcome");
+		System.out.println("\nJava");
+		System.out.println("Programming!");
+	}
+
+}
